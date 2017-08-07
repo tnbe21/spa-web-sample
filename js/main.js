57bc1f603f69a25d1fbb277f11591d9644fe0782
tnbe21 <whpbj809@gmail.com>
2017-08-07 10:01:50 +0000

    fix js/main.js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,21 @@
 
 const API_DOMAIN = 'http://13.112.91.9';
 $(function() {
-  $.get(API_DOMAIN + '/todo/find', (err, result) => {
-    _.each(result, (todo) => {
-      $('li.todo').append(todo.body);
+  $.get(API_DOMAIN + '/todo/find', (todos) => {
+    _.each(todos, (todo) => {
+      $('ul.todo-list').append($('<li>').addClass('todo').append(todo.body));
     });
   });
 
-  $('input.new-todo').on('click', () => {
-    const todo = {
-      body: this.val()
-    };
-    $.post(API_DOMAIN + '/todo/save', todo, (err, result) => {
-      $('li.todo').append(todo.body);
-    });
+  $('input.new-todo').on('keypress', (e) => {
+    if (e.which === 13) {
+      const todo = {
+        body: $(e.target).val()
+      };
+      $.post(API_DOMAIN + '/todo/save', todo, (err, result) => {
+        $('ul.todo-list').append($('<li>').addClass('todo').append(todo.body));
+        $(e.target).val('');
+      });
+    }
   });
 });
